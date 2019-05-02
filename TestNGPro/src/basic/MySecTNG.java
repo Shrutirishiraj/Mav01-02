56ace54bcc4086ebaf5ab278f73422fef2b8beee
shrutirishiraj <shruti@btes.co.in>
2019-05-02 11:48:26 +0530

    testng demo

---
+++ TestNGPro/src/basic/MySecTNG.java
@@ -0,0 +1,12 @@
+package basic;
+
+import org.testng.annotations.Test;
+
+public class MySecTNG {
+	@Test
+	public void m2() {
+		// TODO Auto-generated method stub
+		System.out.println("My testNG class 2: Jenkin Demo");
+	}
+
+}
---
+++ TestNGPro/src/basic/MyfirstTNG.java
@@ -0,0 +1,13 @@
+package basic;
+
+import org.testng.annotations.Test;
+
+public class MyfirstTNG {
+
+	@Test
+	public void m1() {
+		// TODO Auto-generated method stub
+		System.out.println("My testNG class 1: Jenkin Demo");
+	}
+
+}
